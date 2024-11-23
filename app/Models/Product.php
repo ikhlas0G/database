a18c6bb65594e1925d6aba138e11ab7e8f8e8991
Ikhlas Mohammad <ikhlasg2003@gmail.com>
2024-11-23 19:52:21 +0300

    first change

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -7,9 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
+    // hello for somthing new
     protected $table = 'products';
     protected $primaryKey = 'id';
     protected $fillable = ['img', 'name', 'description', 'price'];
-    
+
     use HasFactory;
 }
