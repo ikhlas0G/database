9fb598d501fba7553c466fd5a110947804d0c817
Ikhlas Mohammad <ikhlasg2003@gmail.com>
2024-11-23 19:56:14 +0300

    new change

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    // hello for somthing new
+    // hello for somthing new that
     protected $table = 'products';
     protected $primaryKey = 'id';
     protected $fillable = ['img', 'name', 'description', 'price'];
